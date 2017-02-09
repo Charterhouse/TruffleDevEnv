74677b2b30099cdb0d14e93808e5c099d40c8ddc
Spanbroek, Mark <mark@spanbroek.net>
2017-02-09 16:36:20 +0100

    Revert "Added some code to prove that we can actually call contract code"
    This reverts commit 1e2abb88a976b54b334fc4a38ec2d36f7011a974.

--- local_modules/webapp/app/index.js
+++ local_modules/webapp/app/index.js
@@ -5,32 +5,9 @@ import contract from 'truffle-contract'
 import metacoinJson from 'contracts/build/contracts/MetaCoin.json'
 const MetaCoin = contract(metacoinJson)
 
-import Web3 from 'web3'
+MetaCoin.deployed()
 
-var web3Location = `http://localhost:8545`
-
-window.addEventListener('load', function () {
-  var web3Provided
-  // Supports Metamask and Mist, and other wallets that provide 'web3'.
-  if (typeof web3 !== 'undefined') {
-    // Use the Mist/wallet provider.
-    // eslint-disable-next-line
-    web3Provided = new Web3(web3.currentProvider);
-  } else {
-    web3Provided = new Web3(new Web3.providers.HttpProvider(web3Location))
-  }
-
-  MetaCoin.setProvider(web3Provided.currentProvider)
-  MetaCoin.deployed()
-    .then(function (deployed) {
-      let account = web3Provided.eth.accounts[0]
-      return deployed.getBalanceInEth.call(account, {from: account})
-    })
-    .then(function (balance) {
-      console.debug('Balance: ' + balance.toNumber())
-    })
-
-  ReactDOM.render(
-    <h1>Hello, world!</h1>,
-    document.getElementById('root'))
-})
+ReactDOM.render(
+  <h1>Hello, world!</h1>,
+  document.getElementById('root')
+)

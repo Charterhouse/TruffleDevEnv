98c4ca7d1d904d8b24a8eadc674b9d2fb206dc4d
Stefan van den Oord <stefan.van.den.oord@philips.com>
2017-02-09 14:38:54 +0100

    Fix unused webpack eslint error

--- local_modules/webapp/webpack.config.js
+++ local_modules/webapp/webpack.config.js
@@ -1,4 +1,3 @@
-const webpack = require('webpack')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const WebpackOnBuildPlugin = require('on-build-webpack')
 const replace = require('replace')

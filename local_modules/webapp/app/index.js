e91857261b6e6f351a6d588ff8de29dd7f1a6d2a
Stefan van den Oord <stefan.van.den.oord@philips.com>
2017-02-09 13:50:33 +0100

    Added react

--- local_modules/webapp/app/index.js
+++ local_modules/webapp/app/index.js
@@ -1,5 +1,13 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
 import contract from 'truffle-contract'
 import metacoinJson from 'contracts/build/contracts/MetaCoin.json'
 const MetaCoin = contract(metacoinJson)
 
 MetaCoin.deployed()
+
+ReactDOM.render(
+  <h1>Hello, world!</h1>,
+  document.getElementById('root')
+)

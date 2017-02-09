edc27287be113ef5c4ae7eb2d084cd9ccdea88fb
Spanbroek, Mark <mark@spanbroek.net>
2017-02-09 13:52:05 +0100

    Enable webpack dev server with hot reloading.

--- local_modules/webapp/webpack.config.js
+++ local_modules/webapp/webpack.config.js
@@ -1,13 +1,18 @@
+const webpack = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 const WebpackOnBuildPlugin = require('on-build-webpack')
 const replace = require('replace')
 const path = require('path')
 
 module.exports = {
-  entry: './app/index.js',
+  entry: [
+    './app/index.js'
+  ],
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
   },
+  devtool: 'eval',
   module: {
     loaders: [
       {
@@ -20,7 +25,14 @@ module.exports = {
       }
     ]
   },
+  devServer: {
+    contentBase: 'dist'
+  },
   plugins: [
+    new HtmlWebpackPlugin({
+      inject: true,
+      template: 'index.html'
+    }),
     // Workaround for https://github.com/ethereum/web3.js/issues/555:
     new WebpackOnBuildPlugin(function (stats) {
       replace({

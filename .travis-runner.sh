494070514a4210c8e5c7db4d2a74dbe1d6f12a69
Stefan van den Oord <stefan.van.den.oord@philips.com>
2017-02-09 17:41:32 +0100

    #travis: we need to verify that we’re handling all the installs correctly…

--- .travis-runner.sh
+++ .travis-runner.sh
@@ -4,6 +4,11 @@ startdir="$(cd `dirname $0`; pwd)"
 
 ( cd ${startdir}/local_modules/contracts && yarn install )
 ( cd ${startdir}/local_modules/contracts && yarn run testrpc & )
-( cd ${startdir} && sleep 5 && yarn test )
+( cd ${startdir}/local_modules/webapp && yarn install )
+
+# Make sure testrpc is fully up and running
+sleep 5
+
+( cd ${startdir} && yarn test )
 
 ls ${startdir}/local_modules/webapp/node_modules/.bin

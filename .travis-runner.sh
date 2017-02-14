79a5b6aded4406d813dfb3eb3a5229534565ca2e
Stefan van den Oord <stefan.van.den.oord@philips.com>
2017-02-14 15:26:10 +0100

    Added additional `yarn install` to make sure that all commands are available in Travis

--- .travis-runner.sh
+++ .travis-runner.sh
@@ -6,6 +6,8 @@ startdir="$(cd `dirname $0`; pwd)"
 ( cd ${startdir}/local_modules/contracts && yarn run testrpc & )
 ( cd ${startdir}/local_modules/webapp && yarn install )
 
+yarn install
+
 # Make sure testrpc is fully up and running
 sleep 5
 

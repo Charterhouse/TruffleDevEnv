63676de6e0682af347c76832f0daa5fca1262913
Spanbroek, Mark <mark@spanbroek.net>
2017-03-13 17:29:53 +0100

    Fix error when loading Babel presets & plugins in webpack.

--- src/app-webapp/webpack/babel-module.js
+++ src/app-webapp/webpack/babel-module.js
@@ -1,9 +1,14 @@
+const TransformRuntime = require('babel-plugin-transform-runtime')
+const ES2015 = require('babel-preset-es2015')
+const React = require('babel-preset-react')
+const Stage3 = require('babel-preset-stage-3')
+
 module.exports = {
   test: /\.js$/,
   exclude: /node_modules/,
   loader: 'babel-loader',
   query: {
-    presets: [ 'es2015', 'react', 'stage-3' ],
-    plugins: [ 'transform-runtime' ]
+    presets: [ ES2015, React, Stage3 ],
+    plugins: [ TransformRuntime ]
   }
 }

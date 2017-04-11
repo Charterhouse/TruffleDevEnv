38bfbcd9e3ea74d280bc2360e041e4024d8d3b61
Stefan van den Oord <stefan.van.den.oord@philips.com>
2017-04-11 14:55:39 +0200

    Better way of accessing smart contracts from web code

--- src/app-webapp/app/index.js
+++ src/app-webapp/app/index.js
@@ -3,11 +3,13 @@ import './index.css'
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-import contract from 'truffle-contract'
-import metacoinJson from 'contracts/build/contracts/MetaCoin.json'
-const MetaCoin = contract(metacoinJson)
-
-MetaCoin.deployed()
+/* Now you can do things like this:
+import { MetaCoin, web3 } from './contracts'
+async function doSomethingWithSmartContract() {
+  let deployed = await Metacoin.deployed()
+  ...
+}
+*/
 
 ReactDOM.render(
   <h1>Hello, world!</h1>,

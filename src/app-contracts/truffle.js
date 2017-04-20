f778982716e03d4d8818790192925cb4c45caa42
Spanbroek, Mark <mark@spanbroek.net>
2017-04-20 12:12:37 +0200

    Add Ropsten network configuration to Truffle.

--- src/app-contracts/truffle.js
+++ src/app-contracts/truffle.js
@@ -1,5 +1,10 @@
 module.exports = {
   networks: {
+    ropsten: {
+      host: 'localhost',
+      port: 8546,
+      network_id: 3
+    },
     development: {
       host: 'localhost',
       port: 8545,

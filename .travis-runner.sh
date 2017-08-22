bfc1c9daab921686c707c289ac54750c9bc1c9fc
Spanbroek, Mark <mark@spanbroek.net>
2017-08-22 10:35:34 +0200

    It is now no longer necessary to start TestRPC in continuous integration
    TestRPC is now running in-process when running the smart contract tests.

--- .travis-runner.sh
+++ .travis-runner.sh
@@ -3,13 +3,9 @@
 startdir="$(cd `dirname $0`; pwd)"
 
 ( cd ${startdir}/src/app-contracts && yarn install )
-( cd ${startdir}/src/app-contracts && yarn run testrpc & )
 ( cd ${startdir}/src/app-webapp && yarn install )
 ( cd ${startdir}/src/app-restserver && yarn install )
 
 yarn install
-
-# Make sure testrpc is fully up and running
-sleep 5
 
 ( cd ${startdir} && yarn test )
